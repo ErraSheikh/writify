f81168a153b8cae81899ec1aaf8df761bcc93d90
ErraSheikh <errasheikh01@gmail.com>
2026-05-19 02:04:21 +0500

    update api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,24 @@
+// import axios from 'axios'
+
+// const API = axios.create({
+//     baseURL: 'http://localhost:5000/api'
+// })
+
+// API.interceptors.request.use((req) => {
+//     const user = localStorage.getItem('user')
+//     if(user) {
+//         const data = JSON.parse(user)
+//         req.headers.Authorization = 'Bearer ' + data.token
+//     }
+//     return req
+// })
+
+// export default API
+
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://writify-production-2685.up.railway.app/api'
 })
 
 API.interceptors.request.use((req) => {
