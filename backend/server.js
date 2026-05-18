facdd85b75ce0ff6cfe28451d935a69f0fc9a5ef
ErraSheikh <errasheikh01@gmail.com>
2026-05-19 02:49:39 +0500

    remove port from env

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,20 @@ app.get('/', (req, res) => {
     res.send('Writify server is running')
 })
 
+// mongoose.connect(process.env.MONGO_URI).then(() => {
+//     console.log('MongoDB connected')
+//     app.listen(process.env.PORT || 5000, () => {
+//         console.log('Server started on port 5000')
+//     })
+// }).catch((err) => {
+//     console.log('Connection failed', err)
+// })
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB connected')
-    app.listen(process.env.PORT || 5000, () => {
-        console.log('Server started on port 5000')
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => {
+        console.log('Server started on port ' + PORT)
     })
 }).catch((err) => {
     console.log('Connection failed', err)
