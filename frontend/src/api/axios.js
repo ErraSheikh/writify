fb57320f120e54a53e67344e32b78f6dcf5d4c0c
ErraSheikh <errasheikh01@gmail.com>
2026-06-14 19:20:11 +0500

    code updates

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,4 @@
-// import axios from 'axios'
 
-// const API = axios.create({
-//     baseURL: 'http://localhost:5000/api'
-// })
-
-// API.interceptors.request.use((req) => {
-//     const user = localStorage.getItem('user')
-//     if(user) {
-//         const data = JSON.parse(user)
-//         req.headers.Authorization = 'Bearer ' + data.token
-//     }
-//     return req
-// })
-
-// export default API
 
 import axios from 'axios'
 
